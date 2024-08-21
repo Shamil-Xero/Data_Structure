8fb1882994b67170d6040ede34d462d90d4e5591
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-08-21 07:31:44 +0530

    Other than clone all others work

--- Single_Linked_List.c
+++ Single_Linked_List.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+struct node
+{
     int data;
     struct node *next;
 };
@@ -14,62 +15,74 @@ void DeleteNode();
 void DisplayNodes();
 void SearchNode();
 void SortNodes();
+void CloneLinkedList();
 
-void main() {
+void main()
+{
     int choice;
     printf("\n\n---------- Linked list -----------\n");
     CreateHeadNode();
-    do{
+    do
+    {
         printf("\n\n1. Insert a node\n");
         printf("2. Delete a node\n");
         printf("3. Display the nodes\n");
         printf("4. Search for a node\n");
         printf("5. Sort the nodes\n");
+        printf("6. Clone the Linked List\n");
         printf("0. Exit...\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch(choice){
-            case 1:
-                InsertNode();
-                break;
-            case 2:
-                DeleteNode();
-                break;
-            case 3:
-                DisplayNodes();
-                break;
-            case 4:
-                SearchNode();
-                break;
-            case 5:
-                SortNodes();
-                break;
-            case 0:
-                printf("\nExiting...\n\n");
-                break;
-            default:
-                printf("\n\nInvalid choice. Please try again...\n");
+        switch (choice)
+        {
+        case 1:
+            InsertNode();
+            break;
+        case 2:
+            DeleteNode();
+            break;
+        case 3:
+            DisplayNodes();
+            break;
+        case 4:
+            SearchNode();
+            break;
+        case 5:
+            SortNodes();
+            break;
+        case 6:
+            CloneLinkedList();
+            break;
+        case 0:
+            printf("\nExiting...\n\n");
+            break;
+        default:
+            printf("\n\nInvalid choice. Please try again...\n");
         }
     } while (choice != 0);
 }
 
-void DisplayNodes(){
+void DisplayNodes()
+{
     printf("\n\n--------------------------------\n");
     struct node *current = head;
     printf("Linked List: ");
-    while(current != NULL){
-        printf("(%d)%d(%d) -> ", current, current->data, current->next);
+    while (current != NULL)
+    {
+        printf("%d -> ", current->data);
         current = current->next;
     }
     printf("NULL\n");
     printf("\n\n--------------------------------\n");
 }
 
-void CreateHeadNode(){
+void CreateHeadNode()
+{
     printf("\n\n--------------------------------\n");
     printf("Enter data to create Head node: ");
-    head = (struct node *) malloc(sizeof(struct node));
-    if (head == NULL){
+    head = (struct node *)malloc(sizeof(struct node));
+    if (head == NULL)
+    {
         printf("Memory error!\n");
         return;
     }
@@ -79,79 +92,106 @@ void CreateHeadNode(){
     printf("\n\n--------------------------------\n");
 }
 
-void InsertNode(){
+void InsertNode()
+{
     printf("--------------------------------\n");
     printf("\nEnter the position of the node (-1 to add at the end): ");
     int position, i;
     scanf("%d", &position);
     printf("\nEnter the data of the node: ");
-    struct node *new = (struct node *) malloc(sizeof(struct node));
-    if (new == NULL){
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    if (new == NULL)
+    {
         printf("Memory error!\n");
         return;
-    }else{
+    }
+    else
+    {
         scanf("%d", &new->data);
     }
 
-    if (position == 1) {
+    if (position == 1)
+    {
         new->next = head;
         head = new;
         printf("Node inserted at the start\n");
-    }else if (position < 0){
+    }
+    else if (position < 0)
+    {
         struct node *current = head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
+        {
             current = current->next;
         }
         current->next = new;
         new->next = NULL;
         printf("Node inserted at the end\n");
-    }else {
+    }
+    else
+    {
         struct node *current = head;
-        for (i = 1; i < position - 1; i++) {
-            if (current->next == NULL){
+        for (i = 1; i < position - 1; i++)
+        {
+            if (current->next == NULL)
+            {
                 break;
             }
             current = current->next;
         }
-        if (current->next != NULL){
+        if (current->next != NULL)
+        {
             new->next = current->next;
             current->next = new;
             printf("Node inserted at position: %d\n", position);
-        } else {
+        }
+        else
+        {
             printf("\nPosition Out of range! So node inserted\n");
         }
     }
     DisplayNodes();
 }
 
-void DeleteNode(){
+void DeleteNode()
+{
     printf("--------------------------------\n");
     printf("Enter the position of the node to delete (-1 to delete the last node): ");
     int position;
     scanf("%d", &position);
     struct node *current = head, *previous;
-    if (position == 1){
+    if (position == 1)
+    {
         head = current->next;
         printf("First node is deleted\n");
-    } else if (position < 0){
-        while (current->next != NULL){
+    }
+    else if (position < 0)
+    {
+        while (current->next != NULL)
+        {
             previous = current;
             current = current->next;
         }
         previous->next = NULL;
         printf("Last node is deleted\n");
-    } else {
-        for (int i = 1; i < position; i++){
+    }
+    else
+    {
+        for (int i = 1; i < position; i++)
+        {
             previous = current;
             current = current->next;
-            if (current == NULL){
+            if (current == NULL)
+            {
                 break;
             }
         }
-        if (current != NULL){
+        if (current != NULL)
+        {
             previous->next = current->next;
             printf("\nNode on position %d is deleted\n", position);
-        } else {
+        }
+        else
+        {
             printf("\nPosition Out of range! So node not deleted\n");
         }
     }
@@ -159,28 +199,35 @@ void DeleteNode(){
     DisplayNodes();
 }
 
-void SearchNode(){
+void SearchNode()
+{
     printf("--------------------------------\n");
     printf("Enter the data to search: ");
     int search_data, found = 0, position = 0;
     scanf("%d", &search_data);
     struct node *current = head;
-    while(current != NULL){
+    while (current != NULL)
+    {
         position++;
-        if(current->data == search_data){
+        if (current->data == search_data)
+        {
             found = 1;
             break;
         }
         current = current->next;
     }
-    if(found){
+    if (found)
+    {
         printf("\nNode with data %d found at position %d\n", search_data, position);
-    } else {
+    }
+    else
+    {
         printf("\nNode with data %d not found\n", search_data);
     }
 }
 
-void SortNodes() {
+void SortNodes()
+{
     printf("--------------------------------\n");
     printf("Ascending or descending order (A/D): ");
     char order;
@@ -188,11 +235,15 @@ void SortNodes() {
     printf("\nSorting the linked list...\n");
     struct node *current, *temp, *bubble;
     int buff;
-    if (order == 'A') {
-        for (current = head; current!= NULL; current = current->next) {
+    if (order == 'A')
+    {
+        for (current = head; current != NULL; current = current->next)
+        {
             bubble = current;
-            for (temp = current->next; temp!= NULL; temp = temp->next) {
-                if (temp->data < bubble->data) {
+            for (temp = current->next; temp != NULL; temp = temp->next)
+            {
+                if (temp->data < bubble->data)
+                {
                     bubble = temp;
                 }
             }
@@ -201,11 +252,16 @@ void SortNodes() {
             bubble->data = buff;
         }
         printf("\nLinked list sorted in ascending order.\n");
-    }else if (order == 'D') {
-        for (current = head; current!= NULL; current = current->next) {
+    }
+    else if (order == 'D')
+    {
+        for (current = head; current != NULL; current = current->next)
+        {
             bubble = current;
-            for (temp = current->next; temp!= NULL; temp = temp->next) {
-                if (temp->data > bubble->data) {
+            for (temp = current->next; temp != NULL; temp = temp->next)
+            {
+                if (temp->data > bubble->data)
+                {
                     bubble = temp;
                 }
             }
@@ -214,10 +270,44 @@ void SortNodes() {
             bubble->data = buff;
         }
         printf("\nLinked list sorted in descending order.\n");
-    }else {
+    }
+    else
+    {
         printf("\nInvalid order. Please enter 'A' for ascending or 'D' for descending.\n");
         return;
     }
-    
+
+    DisplayNodes();
+}
+
+void CloneLinkedList()
+{
+    printf("--------------------------------\n");
+    printf("\nCloning the linked list...\n");
+    struct node *current, *new, *cloned_head = NULL;
+    for (current = head; current != NULL; current = current->next)
+    {
+        new = (struct node *)malloc(sizeof(struct node));
+        if (new == NULL)
+        {
+            printf("Memory error!\n");
+            return;
+        }
+        new->data = current->data;
+        if (cloned_head == NULL)
+        {
+            new->next = NULL;
+            cloned_head = new;
+        }
+        else
+        {
+            new->next = NULL;
+        }
+    }
+    for (current = head; current->next != NULL; current = current->next)
+        {
+            continue;
+        }
+        current->next = new;
     DisplayNodes();
 }
