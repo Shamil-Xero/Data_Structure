4364ce12555586a11185f169ad117d06ae790968
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-08-27 22:28:57 +0530

    Add files via upload

---
+++ Circular_Double_Ended_Queue_Arrays.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int front = -1, rear = -1, size;
+
+int *CreateArray(int *array, int size)
+{
+    array = (int *)malloc(size * sizeof(int));
+    return array;
+}
+
+void ShiftLeft(int *array, int size, int index)
+{
+    for (int i = index; i < size; i++)
+    {
+        array[i] = array[i + 1];
+    }
+}
+
+void PrintQueue(int *queue, int size)
+{
+    if (front == -1 && rear == -1)
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
+
+    int i = front;
+    do
+    {
+        printf("%d\t", queue[i]);
+        i = (i + 1) % size;
+    } while (i != (rear + 1) % size);
+    printf("\n");
+}
+
+int AddRight(int *array, int size, int data)
+{
+    if (rear == size - 1)
+    {
+        printf("Queue is full. Cannot enqueue.\n");
+        return -1;
+    }
+
+    if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+        array[rear] = data;
+    }
+    else
+    {
+        rear = (rear + 1) % size;
+        array[rear] = data;
+    }
+
+    return 0;
+}
+
+int AddLeft(int *array, int size, int data)
+{
+
+    if (rear == size - 1)
+    {
+        printf("Queue is full. Cannot enqueue.\n");
+        return -1;
+    }
+
+    if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+        array[front] = data;
+    }
+    else
+    {
+        front = 0;
+        array[front] = data;
+    }
+
+    return 0;
+}
+
+int DelRight(int *array, int size)
+{
+    int data;
+
+    if (front == -1 && rear == -1)
+    {
+        printf("Queue is empty. Cannot dequeue.\n");
+        return -1;
+    }
+
+    data = array[rear];
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        rear = (rear - 1) % size;
+    }
+
+    printf("Dequeued element: %d\n", data);
+}
+
+int DelLeft(int *array, int size)
+{
+    int data;
+
+    if (front == -1 && rear == -1)
+    {
+        printf("Queue is empty. Cannot dequeue.\n");
+        return -1;
+    }
+
+    data = array[front];
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = (front + 1) % size;
+    }
+
+    printf("Dequeued element: %d\n", data);
+    return 0;
+}
+
+int main()
+{
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int *queue = CreateArray(queue, size);
+
+    do
+    {
+        printf("\n1. Display\n2. AddRight\n3. AddLeft\n4. DelRight\n5. DelLeft\n0. Exit\n");
+        printf("Your choice: ");
+        int choice, data;
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            PrintQueue(queue);
+            break;
+        case 2:
+            printf("Enter the data: ");
+            scanf("%d", &data);
+            AddRight(queue, size, data);
+            break;
+        case 3:
+            printf("Enter the data: ");
+            scanf("%d", &data);
+            AddLeft(queue, size, data);
+            break;
+        case 4:
+            DelRight(queue, size);
+            break;
+        case 5:
+            DelLeft(queue, size);
+            break;
+        case 0:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please try again.\n");
+        }
+    } while (1);
+
+    free(queue);
+}
