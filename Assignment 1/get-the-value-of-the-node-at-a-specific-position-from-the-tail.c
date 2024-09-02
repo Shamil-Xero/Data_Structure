b26877357e6b26d6d38e41f27f06ecf85f331dd7
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:39:38 +0530

    Create get-the-value-of-the-node-at-a-specific-position-from-the-tail.c

---
+++ Assignment 1/get-the-value-of-the-node-at-a-specific-position-from-the-tail.c
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem?isFullScreen=false
+
+int getNode(SinglyLinkedListNode* llist, int positionFromTail) {
+    SinglyLinkedListNode *current= llist;
+    int num_nodes=0;
+    if(llist!=NULL){
+        while(current!=NULL){
+            num_nodes++;
+            current=current->next;
+        }
+    }
+    current= llist;
+    for(int i=1; i<num_nodes-positionFromTail; i++){
+        current=current->next;
+    }
+    return current->data;
+}
