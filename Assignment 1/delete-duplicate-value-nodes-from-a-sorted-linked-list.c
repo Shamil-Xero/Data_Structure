fcce415e1cb605d7fd75c0a63c4a084be070e484
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:44:22 +0530

    Update delete-duplicate-value-nodes-from-a-sorted-linked-list.c

--- Assignment 1/delete-duplicate-value-nodes-from-a-sorted-linked-list.c
+++ Assignment 1/delete-duplicate-value-nodes-from-a-sorted-linked-list.c
@@ -1,16 +1,22 @@
-//https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list/problem?isFullScreen=false
+// https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list/problem?isFullScreen=false
 
-SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
-    if (llist == NULL || llist->next == NULL) {
+SinglyLinkedListNode *removeDuplicates(SinglyLinkedListNode *llist)
+{
+    if (llist == NULL || llist->next == NULL)
+    {
         return llist;
     }
 
     SinglyLinkedListNode *current = llist, *next_node = current->next;
 
-    while (next_node != NULL) {
-        if (current->data == next_node->data) {
+    while (next_node != NULL)
+    {
+        if (current->data == next_node->data)
+        {
             current->next = next_node->next;
-        } else {
+        }
+        else
+        {
             current = next_node;
         }
         next_node = next_node->next;
