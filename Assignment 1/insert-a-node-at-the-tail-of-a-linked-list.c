9d1da5495b5d95cfae2b4848ddaef634f00b2381
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:42:41 +0530

    Update insert-a-node-at-the-tail-of-a-linked-list.c

--- Assignment 1/insert-a-node-at-the-tail-of-a-linked-list.c
+++ Assignment 1/insert-a-node-at-the-tail-of-a-linked-list.c
@@ -1,18 +1,22 @@
-//https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=false
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=false
 
-SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+SinglyLinkedListNode *insertNodeAtTail(SinglyLinkedListNode *head, int data)
+{
     SinglyLinkedListNode *new = (SinglyLinkedListNode *)malloc(sizeof(SinglyLinkedListNode));
     SinglyLinkedListNode *current = head;
     new->data = data;
     new->next = NULL;
-    if(head==NULL){
-        head=new;
-    }else{
+    if (head == NULL)
+    {
+        head = new;
+    }
+    else
+    {
         while (current->next != NULL)
         {
             current = current->next;
         }
-    current->next = new;
+        current->next = new;
     }
     return head;
 }
