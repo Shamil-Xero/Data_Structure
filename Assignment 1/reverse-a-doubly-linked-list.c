80a54df44db36a471709a2bdaa1aace549484114
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:38:16 +0530

    Create reverse-a-doubly-linked-list.c

---
+++ Assignment 1/reverse-a-doubly-linked-list.c
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem?isFullScreen=false
+
+DoublyLinkedListNode* reverse(DoublyLinkedListNode* llist) {
+    DoublyLinkedListNode *current = llist, *temp, *tail;
+    for(DoublyLinkedListNode *current = llist; current!=NULL; current=current->next){
+        tail=current;
+    }
+    tail = llist;
+    for (current = llist; current != NULL; current = current->prev)
+    {
+        temp = current->next;
+        current->next = current->prev;
+        current->prev = temp;
+        llist = current;
+    }
+    return llist;
+}
