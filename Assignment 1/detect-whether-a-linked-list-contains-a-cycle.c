ae4a20d55e08a4b6dc6040a98bb29b06feff9c55
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:44:37 +0530

    Update detect-whether-a-linked-list-contains-a-cycle.c

--- Assignment 1/detect-whether-a-linked-list-contains-a-cycle.c
+++ Assignment 1/detect-whether-a-linked-list-contains-a-cycle.c
@@ -1,19 +1,23 @@
-//https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem?isFullScreen=false
+// https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem?isFullScreen=false
 
-bool has_cycle(SinglyLinkedListNode* head) {
-    SinglyLinkedListNode *temp= head;
-    int add[1000],i=0;
-    add[i]=(int)temp;
-    while(temp->next!=NULL){
+bool has_cycle(SinglyLinkedListNode *head)
+{
+    SinglyLinkedListNode *temp = head;
+    int add[1000], i = 0;
+    add[i] = (int)temp;
+    while (temp->next != NULL)
+    {
         int j;
-        temp=temp->next;
-        for(j=0;j<=i;j++){
-            if((int)temp==add[j]){
+        temp = temp->next;
+        for (j = 0; j <= i; j++)
+        {
+            if ((int)temp == add[j])
+            {
                 return 1;
             }
         }
         i++;
-        add[i]=(int)temp;
+        add[i] = (int)temp;
     }
     return 0;
 }
