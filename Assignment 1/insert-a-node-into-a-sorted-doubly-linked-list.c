f5b57833d8964a3bdd4822c20265980f50062c89
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:43:00 +0530

    Update insert-a-node-into-a-sorted-doubly-linked-list.c

--- Assignment 1/insert-a-node-into-a-sorted-doubly-linked-list.c
+++ Assignment 1/insert-a-node-into-a-sorted-doubly-linked-list.c
@@ -1,24 +1,29 @@
-//https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem?isFullScreen=false
+// https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem?isFullScreen=false
 
-DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data) {
-    DoublyLinkedListNode* new = (DoublyLinkedListNode *)malloc(sizeof(DoublyLinkedListNode));
-    new->data= data;
+DoublyLinkedListNode *sortedInsert(DoublyLinkedListNode *llist, int data)
+{
+    DoublyLinkedListNode *new = (DoublyLinkedListNode *)malloc(sizeof(DoublyLinkedListNode));
+    new->data = data;
 
-    if (llist == NULL || llist->data >= data) {
+    if (llist == NULL || llist->data >= data)
+    {
         new->next = llist;
-        if (llist != NULL) {
+        if (llist != NULL)
+        {
             llist->prev = new;
         }
         return new;
     }
 
-    DoublyLinkedListNode* current = llist;
-    while (current->next != NULL && current->next->data < data) {
+    DoublyLinkedListNode *current = llist;
+    while (current->next != NULL && current->next->data < data)
+    {
         current = current->next;
     }
 
     new->next = current->next;
-    if (current->next != NULL) {
+    if (current->next != NULL)
+    {
         current->next->prev = new;
     }
     current->next = new;
