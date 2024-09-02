b308335fddc887e8962223a73e7d8e3b7d03560c
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:43:23 +0530

    Update print-the-elements-of-a-linked-list.c

--- Assignment 1/print-the-elements-of-a-linked-list.c
+++ Assignment 1/print-the-elements-of-a-linked-list.c
@@ -1,7 +1,9 @@
-void printLinkedList(SinglyLinkedListNode* head) {
-    for(SinglyLinkedListNode *current = head; current!=NULL; current=current->next){
+// https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=false
+
+void printLinkedList(SinglyLinkedListNode *head)
+{
+    for (SinglyLinkedListNode *current = head; current != NULL; current = current->next)
+    {
         printf("%d\n", current->data);
     }
 }
-
-//https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=false
