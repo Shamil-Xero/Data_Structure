497873ef2724467b18458ea4f7fd99766ebda6d5
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:43:58 +0530

    Update delete-a-node-from-a-linked-list.c

--- Assignment 1/delete-a-node-from-a-linked-list.c
+++ Assignment 1/delete-a-node-from-a-linked-list.c
@@ -1,14 +1,16 @@
-//https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem?isFullScreen=false
+// https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem?isFullScreen=false
 
-SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+SinglyLinkedListNode *deleteNode(SinglyLinkedListNode *llist, int position)
+{
     SinglyLinkedListNode *current = llist;
-    if(position == 0){
+    if (position == 0)
+    {
         llist = llist->next;
     }
-    for (int i = 0; i < position-1 ; i++)
-        {
-            current = current->next;
-        }
-        current->next=current->next->next;
+    for (int i = 0; i < position - 1; i++)
+    {
+        current = current->next;
+    }
+    current->next = current->next->next;
     return llist;
 }
