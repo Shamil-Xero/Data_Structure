ad9dd621a8361300816f35f1cd4517fa733168cc
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-09-02 21:45:14 +0530

    Update insert-a-node-at-a-specific-position-in-a-linked-list.c

--- Assignment 1/insert-a-node-at-a-specific-position-in-a-linked-list.c
+++ Assignment 1/insert-a-node-at-a-specific-position-in-a-linked-list.c
@@ -1,7 +1,8 @@
-//https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem?isFullScreen=false
+// https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem?isFullScreen=false
 
-SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
-    SinglyLinkedListNode* current = llist;
+SinglyLinkedListNode *insertNodeAtPosition(SinglyLinkedListNode *llist, int data, int position)
+{
+    SinglyLinkedListNode *current = llist;
     SinglyLinkedListNode *new = (SinglyLinkedListNode *)malloc(sizeof(SinglyLinkedListNode));
     new->data = data;
     for (int i = 1; i < position; i++)
