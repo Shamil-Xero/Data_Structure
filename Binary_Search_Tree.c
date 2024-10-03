947a3233d28547adbc9a9b09e2cb136bab5d7b9c
Shamil Irfan <133078420+Shamil-Xero@users.noreply.github.com>
2024-10-03 22:18:03 +0530

    Add files via upload

--- Binary_Search_Tree.c
+++ Binary_Search_Tree.c
@@ -2,51 +2,113 @@
 #include <stdlib.h>
 
 // Structure for a tree node
-struct Node {
+typedef struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
-};
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+Node *root = NULL;
 
 // Creating a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+Node *createNode(int data)
+{
+    Node *new = (Node *)malloc(sizeof(Node));
+    new->data = data;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
 }
 
 // Inserting a node in the BST
-struct Node* insert(struct Node* root, int data) {
-    if (root == NULL) {
+Node *insert(Node *root, int data)
+{
+    if (root == NULL)
+    {
         return createNode(data);
     }
-    if (data < root->data) {
-        root->left = insert(root->left, data);
-    } else {
-        root->right = insert(root->right, data);
+    if (data < (root)->data)
+    {
+        (root)->left = insert((root)->left, data);
+    }
+    else
+    {
+        (root)->right = insert((root)->right, data);
     }
     return root;
 }
 
-// In-order traversal of the BST
-void inOrderTraversal(struct Node* root) {
-    if (root != NULL) {
+// Function to find the node with the minimum value in the BST
+Node *minValue(Node *node)
+{
+    Node *current = node;
+    while (current && current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current;
+}
+
+// Function to delete a node from the BST
+Node *delete(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (data < root->data)
+    {
+        root->left = delete(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = delete(root->right, data);
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        Node *temp = minValue(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, temp->data);
+    }
+    return root;
+}
+
+// Traversal of the BST
+void inOrderTraversal(Node *root)
+{
+    if (root != NULL)
+    {
         inOrderTraversal(root->left);
         printf("%d ", root->data);
         inOrderTraversal(root->right);
     }
 }
-void preOrderTraversal(struct Node* root) {
-    if (root != NULL) {
+void preOrderTraversal(Node *root)
+{
+    if (root != NULL)
+    {
         printf("%d ", root->data);
         inOrderTraversal(root->left);
         inOrderTraversal(root->right);
     }
 }
-void postOrderTraversal(struct Node* root) {
-    if (root != NULL) {
+void postOrderTraversal(Node *root)
+{
+    if (root != NULL)
+    {
         inOrderTraversal(root->left);
         inOrderTraversal(root->right);
         printf("%d ", root->data);
@@ -54,28 +116,37 @@ void postOrderTraversal(struct Node* root) {
 }
 
 // Main function
-int main() {
-    struct Node* root = NULL;
+int main()
+{
     int n, data;
 
-    printf("Enter the number of nodes to insert: ");
-    scanf("%d", &n);
+    // printf("Enter the number of nodes to insert: ");
+    // scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter value to insert: ");
-        scanf("%d", &data);
-        root = insert(root, data);
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("Enter value to insert: ");
+    //     scanf("%d", &data);
+    //     root = insert(root, data);
+    // }
 
-    printf("\nIn-order traversal of the BST: ");
+    root = insert(root, 2);
+    root = insert(root, 65);
+    root = insert(root, 4);
+    root = insert(root, 21);
+    printf("\n\nIn-order traversal of the BST: ");
     inOrderTraversal(root);
-    printf("\n");
-    printf("\nPre-order traversal of the BST: ");
-    preOrderTraversal(root);
-    printf("\n");
-    printf("\nPost-order traversal of the BST: ");
-    postOrderTraversal(root);
-    printf("\n");
+    root = delete(root, 2);
+    root = delete(root, 65);
+    root = insert(root, 42);
+    root = insert(root, 3);
+    printf("\n\nIn-order traversal of the BST: ");
+    inOrderTraversal(root);
+
+    // printf("\n\nPre-order traversal of the BST: ");
+    // preOrderTraversal(root);
+    // printf("\n\nPost-order traversal of the BST: ");
+    // postOrderTraversal(root);
 
     return 0;
 }
